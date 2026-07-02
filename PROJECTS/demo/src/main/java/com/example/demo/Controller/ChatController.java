ff910b3e1775ecd3d41c7b061bd53d06ecad731d
Srinibas Khuntia <srinibaskhuntia39@gmail.com>
2026-07-02 21:08:32 +0530

    ChatClientBuilder

---
+++ PROJECTS/demo/src/main/java/com/example/demo/Controller/ChatController.java
@@ -0,0 +1,20 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Service.ChatService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+    private final ChatService chatService;
+
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+    @RequestMapping("/chat")
+    public String chat(@RequestParam String message){
+        return chatService.chat(message);
+    }
+}
